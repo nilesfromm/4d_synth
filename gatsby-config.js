377db220c36369d2f622fc93245ac660c487a3ea
Niles <nsfromm@gmail.com>
2021-05-02 11:16:19 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "synth",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-K1VF8M0DEM",
+      },
+    },
+  ],
+};
